02456341503004ddcb649a116c3b6bf6a5b4baec
Leon Weidauer <leon.weidauer@googlemail.com>
2014-01-21 14:38:15 +0100

    fixed missing quotes

--- bash_includes/functions.sh
+++ bash_includes/functions.sh
@@ -1,22 +1,22 @@
 # usage: lines 11 derp.txt     #get line 11
 # or:    lines 5 14 derp.txt   #get lines 5-14
 function lines {
-    if [[ ! -z $3 ]]; then
-        sed -n "$1,$2p" $3
+    if [[ ! -z "$3" ]]; then
+        sed -n "$1,$2p" "$3"
     else
-        sed -n "$1p" $2
+        sed -n "$1p" "$2"
     fi
 }
 
 # usage: notify "derp is derping"         # just a message
 # or:    notify "derp" "derp is derping"  # title and message
 function notify {
-    if [[ ! -z $2 ]]; then
-        title=$1
-        message=$2
+    if [[ ! -z "$2" ]]; then
+        title="$1"
+        message="$2"
     else
         title="Notification"
-        message=$1
+        message="$1"
     fi
 
     if [[ $(which notify-send) ]]; then
@@ -25,7 +25,6 @@ function notify {
     elif [[ $(which osascript) ]]; then
         # mac osx version
         as_command="display notification \"$message\" with title \"$title\""
-        echo $as_command
         osascript -e "$as_command"
     else
         # terminal fallback version
@@ -37,7 +36,7 @@ function notify {
 
 # usage: remind 5m "Tea is ready"
 function remind {
-    if [ ! -z $2 ]; then
+    if [ ! -z "$2" ]; then
         sleep $1 && notify "Reminder" "$2" > /dev/null 2>&1 &
     else
         echo "Usage: remind 15m 'you gotta leave!'"

1b6499eaf9797da2eac31062f2777881fd4a4c8c
Leon Weidauer <leon.weidauer@googlemail.com>
2014-01-24 15:25:15 +0100

    moved stuff to local bashrc

--- bash_includes/path.sh
+++ bash_includes/path.sh
@@ -2,5 +2,3 @@
 # Add RVM to PATH for scripting
 PATH=$PATH:$HOME/.rvm/bin 
 
-# Android SDK
-PATH=${PATH}:/Users/leon/Applications/android_sdk/platform-tools:/Users/leon/Applications/android_sdk/tools

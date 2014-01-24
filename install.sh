6457864f0b15c418be21129d5019505142d10b0f
Leon Weidauer <leon.weidauer@googlemail.com>
2014-01-24 15:54:36 +0100

    added install script

---
+++ install.sh
@@ -0,0 +1,27 @@
+DIR=$(pwd)
+
+echo "Updating submodules ..."
+git submodule update --init --recursive
+
+cd ~
+
+LINKS='bashrc
+bash_profile
+inputrc
+vimrc
+gitconfig
+gitignore_global
+vim'
+
+for L in $LINKS; do
+    # check if the file/dir/link exists and backup the existing version
+    if [[ -e ".$L" ]]; then
+        echo "Backing up .$L"
+        mv ".$L" ".$L.backup"
+    fi
+    echo "Linking .$L"
+    ln -s "$DIR/$L" ".$L"
+    echo
+done
+
+cd $OLD_PWD

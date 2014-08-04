785e13f832a166a22a9eb4aefb57ea1485fe95fe
Leon Weidauer <leon@lnwdr.de>
2014-08-04 16:40:39 +0200

    Update git.sh

--- bash_includes/git.sh
+++ bash_includes/git.sh
@@ -2,7 +2,7 @@
 # GIT     #
 ###########
 export GIT_AUTHOR_NAME="Leon Weidauer"
-export GIT_AUTHOR_EMAIL="leon.weidauer@googlemail.com"
+export GIT_AUTHOR_EMAIL="leon@lnwdr.de"
 export GIT_COMMITTER_NAME="$GIT_AUTHOR_NAME"
 export GIT_COMMITTER_EMAIL="$GIT_AUTHOR_EMAIL"
 

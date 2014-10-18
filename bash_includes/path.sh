f2c85885bd3573f4085540c13fc65a7ae85d0fbf
Leon Weidauer <leon@lnwdr.de>
2014-10-18 11:27:26 +0200

    added homebrew paths

--- bash_includes/path.sh
+++ bash_includes/path.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+PATH=$PATH:$HOME/usr/local/bin # Homebrew bin
+PATH=$PATH:$HOME/usr/local/sbin # Homebrew sbin
 PATH=$PATH:/usr/local/share/npm/bin # NPM bins
 
